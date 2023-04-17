b456c9e1b0712cc75bf86ab591c48353e71703cb
KoryahSiha <koryahsiha@gmail.com>
2023-04-17 09:37:44 +0200

    Séparation des admins des autres fixtures
    Les admins et autres fixtures sont désormais séparés en deux groupes lors des chargements des fixtures avec l'utilisation de FixtureGroupInterface.

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,65 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Bundle\FixturesBundle\FixtureGroupInterface;
+use Doctrine\Persistence\ManagerRegistry;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory as FakerFactory;
+use Faker\Generator as FakerGenerator;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
-class AppFixtures extends Fixture
-{
-    public function load(ObjectManager $manager): void
+class AppFixtures extends Fixture implements FixtureGroupInterface
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        private $faker;
+        private $hasher;
+        private $manager;
 
-        $manager->flush();
+        public static function getGroups(): array
+        {
+            return ['group1'];
+        }
+
+        public function __construct(ManagerRegistry $doctrine, UserPasswordHasherInterface $hasher)
+        {
+            $this->doctrine = $doctrine;
+            $this->faker = FakerFactory::create('fr_FR');
+            $this->hasher = $hasher;
+
+        }
+
+        public function load(ObjectManager $manager): void
+        {
+            $this->manager = $manager;
+
+            // @infos, les admins possèdent le rôle admin
+            $this->loadUsers();
+        }
+
+        public function loadUsers(): void
+        {
+            $datas = [
+                [
+                    'email' => 'admin@example.com',
+                    'password' => '123',
+                    'roles' => ['ROLE_ADMIN'],
+                    'enabled' => true
+                ],
+            ];
+
+        foreach ($datas as $data) {
+            $user = new User();
+
+            $user->setEmail($data['email']);
+            $password = $this->hasher->hashPassword($user, $data['password']);
+            $user->setPassword($password);
+            $user->setRoles($data['roles']);
+            $user->setEnabled($data['enabled']);
+            
+            $this->manager->persist($user);
+        };
+
+        $this->manager->flush();
+        }
     }
-}
