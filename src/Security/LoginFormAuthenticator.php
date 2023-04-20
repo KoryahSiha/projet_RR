50f5b4b44f3c648d4b900baa5a345d8abf42eb2c
KoryahSiha <koryahsiha@gmail.com>
2023-04-20 09:20:48 +0200

    Modification des redirections une fois connecté.

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -48,18 +48,18 @@ class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
 
         $user = $token->getUser();
         $roles = $user->getRoles();
-
+        
         if (in_array('ROLE_ADMIN', $roles)) {
             // @todo si l'utilisateur est un admin, une fois la connexion validée, renvoie vers le dashboard
             return new RedirectResponse($this->urlGenerator->generate
-            ('app_salle_index'));
+            ('app_reservation_index'));
         } else {
-            // if (!$user->getGestionnaireSalle()) {
-            //     throw new Exception("Le compte utilisateur {$user->getId()} n'est relié à aucun student");
+            /// if (!$user->getGestionnaireSalle()) {
+            //     throw new Exception("Le compte utilisateur {$user->getId()} n'est relié à aucun gestionnaire de salle");
             // }
             // @todo si l'utilisateur n'est pas un admin, une fois la connexion validée, renvoie vers la page d'accueil de l'agenda
             return new RedirectResponse($this->urlGenerator->generate
-            ('app_salle', [
+            ('app_gestionnaire_salle_show', [
                 'id' => $user->getGestionnaireSalle()->getId(),
             ]));
         }
