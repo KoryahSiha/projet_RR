d710e8bbc2934ea9ba00890c428afb7ea18a97e6
KoryahSiha <koryahsiha@gmail.com>
2023-05-02 16:49:11 +0200

    Modification du fichier `LoginFormAuthenticator.php`
    Une fois logués, les admins sont désormais redirigés vers le dashboard.

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -52,7 +52,7 @@ class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
         if (in_array('ROLE_ADMIN', $roles)) {
             // @todo si l'utilisateur est un admin, une fois la connexion validée, renvoie vers le dashboard
             return new RedirectResponse($this->urlGenerator->generate
-            ('app_reservation_index'));
+            ('admin'));
         } else {
             /// if (!$user->getGestionnaireSalle()) {
             //     throw new Exception("Le compte utilisateur {$user->getId()} n'est relié à aucun gestionnaire de salle");
