ef0282f97e7442ec0bfb76c9d40018b49b76fcf3
KoryahSiha <koryahsiha@gmail.com>
2023-06-28 16:07:16 +0200

    Récupération des réservations
    Les réservations sont récupérées et les données converties au format JSON afin d'être injectées ensuite dans l'agenda.

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,42 @@
 
 namespace App\Controller;
 
+use App\Repository\ReservationRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home', methods: ['GET'])]
-    public function home()
+    #[Route('/', name: 'app_home', methods: ['GET', 'POST'])]
+    public function home(ReservationRepository $reservation)
     {
-        // affichage d'une vue sans transmission de variables
-        return $this->render('pages/home.html.twig');
+        $reservations = $reservation->findAll();
+        // dd($reservations);
+        $events = [];
+
+        foreach($reservations as $reservation) {
+            $events[] = [
+                'id' => $reservation->getId(),
+                'start' => $reservation->getStart()->format('Y-m-d H:i:s'),
+                'end' => $reservation->getEnd()->format('Y-m-d H:i:s'),
+                'title' => $reservation->getTitle(),
+                'description' => $reservation->getDescription(),
+                'typeReservation' => $reservation->getTypeReservation(),
+                'gestionnaireSalle' => $reservation->getGestionnaireSalle(),
+                'participantNumber' => $reservation->getParticipantNumber(),
+                'duration' => $reservation->getDuration(),
+                'backgroundColor' => $reservation->getBackgroundColor(),
+                'borderColor' => $reservation->getBorderColor(),
+                'textColor' => $reservation->getTextColor(),
+                'allDay' => $reservation->getAllDay(),
+                'salle' => $reservation->getSalle(),
+            ];
+        }
+
+        $data = json_encode($events);
+
+        return $this->render('pages/home.html.twig', ['data' => $data]);
     }
 
 }
