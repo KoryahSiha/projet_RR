9e75e589e48663db37d901c54a0b3044cc961fa5
KoryahSiha <koryahsiha@gmail.com>
2023-07-17 14:09:19 +0200

    Modification du fichier app.js

--- assets/app.js
+++ assets/app.js
@@ -5,7 +5,6 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
-import './js/calendar';
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
